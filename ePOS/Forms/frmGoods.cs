e505e42be8dab8d469fa3e5931a3c0dd6d79e514
obuyajr <felix37brock@gmail.com>
2024-04-10 15:04:18 +0300

    front office starts

--- ePOS/Forms/frmGoods.cs
+++ ePOS/Forms/frmGoods.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Xml.Linq;
+
 
 namespace ePOS.Forms
 {
@@ -16,5 +20,28 @@ namespace ePOS.Forms
         {
             InitializeComponent();
         }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+            
+            // Define the insert query with parameters
+            StrCmd = "INSERT INTO goods (, Age) VALUES (@Name, @Age)";
+            
+
+                // Create a SqlCommand object
+                using (cmd = new SqlCommand(insertQuery, ))
+{
+                // Add parameters to prevent SQL injection
+                    cmd.Parameters.AddWithValue("@Name", );
+                    cmd.Parameters.AddWithValue("@Age", age);
+
+                    // Execute the query
+                    cmd.ExecuteNonQuery();
+
+                    // Show success message (optional)
+                    MessageBox.Show("Data inserted successfully!");
+                }
+            }
     }
 }
--- ePOS/modules/sysPublic1.cs
+++ ePOS/modules/sysPublic1.cs
@@ -1,31 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ePOS.modules
 {
-    
-
-    class sysPublic
+    public class sharedData
     {
         public static string loggedInUserName = "";
-        public SqlConnection conn = new SqlConnection();
-        public string StrCmd;
+        public static SqlConnection conn = new SqlConnection();
+        public static string StrCmd;
         public SqlCommand Cmd = new SqlCommand();
         public int loggedInActiveUserID = 0;
         public string loggedInUserAccNo = "";
 
+
         public static SqlConnection getDatabaseConnection()
         {
             string connString = "Data Source=felix;Initial Catalog=schoolManagementSys;User ID=felix;Password=felix37brock;";
-            SqlConnection conn = new SqlConnection(connString);
+            conn.ConnectionString = connString;
             conn.Open();
 
             return conn;
         }
-
     }
 }
